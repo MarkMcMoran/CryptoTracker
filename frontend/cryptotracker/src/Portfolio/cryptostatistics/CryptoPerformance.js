77ed6308885921ff96e63818d8dbff4e8a62207c
Mark McMoran <mark.mcmoran@live.com>
2021-07-29 12:56:27 +0100

    Adding the deps & up to date settings

---
+++ frontend/cryptotracker/src/Portfolio/cryptostatistics/CryptoPerformance.js
@@ -0,0 +1,28 @@
+import React from "react";
+import {Statistic, Row, Col} from 'antd';
+import {ArrowUpOutlined, ArrowDownOutlined} from '@ant-design/icons';
+import {Card} from "@material-ui/core";
+import './CryptoAmount.css';
+
+const CryptoPerformance = () => {
+
+    return (
+        <div className="Performance-card">
+            <Row gutter={16}>
+                <Col span={12}>
+                    <Card>
+                        <Statistic
+                            title="Overall performance (YTD)"
+                            value={15}
+                            valueStyle={{color: '#3f8600'}}
+                            prefix={<ArrowUpOutlined/>}
+                            suffix="%"
+
+                        />
+                    </Card>
+                </Col>
+            </Row>
+        </div>);
+}
+
+export default CryptoPerformance;
